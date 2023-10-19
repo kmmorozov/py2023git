8c0ada74d02406c71fadf262ad3efc3cb6812428
morozovkm <morozovkm@yandex.ru>
2023-10-19 20:55:50 +0300

    Добавил файл calculater.py - отстов программы

---
+++ calculater.py
@@ -0,0 +1,13 @@
+a = int(input('Введите число а: '))
+
+b = int(input('Введите число b: '))
+
+
+action = input( 'Введите действие. Допустимо: plus, minus, koren, stepen, umnoj, deli, sred, min, max, sumX2  : ')
+
+if action == 'plus':
+    result = a + b
+    print(result)
+elif action == 'minus':
+    result = a - b
+    print(result)
