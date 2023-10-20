cfa2637bcdf1875d2cd2e397e9d363fe26b0c086
Chiken <shmakov23@gmail.com>
2023-10-20 09:16:13 +0300

    исправил логику фукции koren корень числа находится, но значение не возвращается

--- ValeriySh.py
+++ ValeriySh.py
@@ -1,6 +1,10 @@
 # koren
 from math import sqrt
 def koren(a, b):
-    sqrt(a)
-    sqrt(b)
-    return(a,b)
+    a = sqrt(a)
+    print(a)
+    b = sqrt(b)
+    print(b)
+    return a, b
+    # return c1, c2
+
