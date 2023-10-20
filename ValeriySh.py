3c37c85d0ee09a8937ae103a57aee8c52479db1e
Chiken <shmakov23@gmail.com>
2023-10-20 10:52:32 +0300

    исправил немного логику фукции koren результат функции выводится внутри функции, результат все также не возвращается в программу

--- ValeriySh.py
+++ ValeriySh.py
@@ -2,9 +2,10 @@
 from math import sqrt
 def koren(a, b):
     a = sqrt(a)
-    print(a)
+    # print(a)
     b = sqrt(b)
-    print(b)
+    # print(b)
+    print("Корень из А: ", a, "\nКорень из B: ", b)
     return a, b
     # return c1, c2
 
