ebc78faae4c450434ed62a9ffc693d2165b3d9f3
vailiy <vase198802@gmail.com>
2023-10-22 11:03:41 +0300

    добавил файл del.py

---
+++ del.py
@@ -0,0 +1,7 @@
+print("введите два числа")
+def my_deli(a,b):
+    a = int(input('Введите число а: '))
+    b = int(input('Введите число b: '))
+    if action == 'deli':
+        result = a / b
+        print(result)
