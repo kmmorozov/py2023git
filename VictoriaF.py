7aa4bee16ddb2abecf24f7972612979036ca5d00
ChumaTV <chumatv@gmail.com>
2023-10-23 19:33:12 +0300

    Удалила лишний код

--- VictoriaF.py
+++ VictoriaF.py
@@ -1,9 +1,3 @@
-print("Вас приветствует Калькулятор умножения")
-print("Пожалуйста, введите два числа")
-
-a = int(input("Введите первое число а: "))
-b = int(input("Введите второе число b: "))
-
 def my_multiplication(a,b)
     result = a * b
     return result
